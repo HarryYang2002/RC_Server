c253b6edb225110cc71458c1bf702d51540aa4cb
HarryYang2002 <1260828355@qq.com>
2022-09-24 22:45:51 +0800

    server练习

---
+++ client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	trippb "server/protoc/gen/go"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	conn, err := grpc.Dial("localhost:8081", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("cannot connect server: %v",err)
+	}
+	tsClient := trippb.NewTripServiceClient(conn)
+	r, err := tsClient.GetTrip(context.Background(), &trippb.GetTripRequest{
+		Id: "trip456",
+	})
+	if err != nil {
+		log.Fatalf("cannto call GetTrip: %v",err)
+	}
+
+	fmt.Println(r)
+}
