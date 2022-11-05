0dbb773f9231b417cd8e895c06f46e92e2714843
HarryYang2002 <1260828355@qq.com>
2022-11-05 18:42:21 +0800

    位置模拟服务的使用

--- cmd/aiclient/main.go
+++ cmd/aiclient/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"server/car/mq/amqpclt"
 	coolenvpb "server/shared/coolenv"
+	"server/shared/server"
+	"time"
 )
 
 func main() {
@@ -30,4 +34,75 @@ func main() {
 		panic(any(err))
 	}
 	fmt.Printf("%+v\n", res)
+
+	idRes, err := ac.LicIdentity(c, &coolenvpb.IdentityRequest{
+		Photo: []byte{1, 2, 3, 4, 5},
+	})
+	if err != nil {
+		panic(any(err))
+	}
+	fmt.Printf("%+v\n", idRes)
+
+	_, err = ac.SimulateCarPos(c, &coolenvpb.SimulateCarPosRequest{
+		CarId: "car123",
+		InitialPos: &coolenvpb.Location{
+			Latitude:  30,
+			Longitude: 120,
+		},
+		//Type: coolenvpb.PosType_RANDOM, //随机数字
+		Type: coolenvpb.PosType_NINGBO,
+	})
+	if err != nil {
+		panic(any(err))
+	}
+
+	logger, err := server.NewZapLogger()
+	if err != nil {
+		panic(any(err))
+	}
+
+	amqpConn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(any(err))
+	}
+
+	sub, err := amqpclt.NewSubscriber(amqpConn, "pos_sim", logger)
+	if err != nil {
+		panic(any(err))
+	}
+
+	ch, cleanUp, err := sub.SubscribeRaw(c)
+	defer cleanUp()
+
+	if err != nil {
+		panic(any(err))
+	}
+
+	tm := time.After(10 * time.Second)
+	for {
+		shouldStop := false
+		select {
+		case msg := <-ch:
+			fmt.Printf("%s\n", msg.Body)
+			//var update coolenvpb.CarPosUpdate
+			//err = json.Unmarshal(msg.Body, &update)
+			//if err != nil {
+			//	panic(err)
+			//}
+			//fmt.Printf("%+v\n", &update)
+		case <-tm:
+			shouldStop = true
+		}
+		if shouldStop {
+			break
+		}
+	}
+
+	_, err = ac.EndSimulateCarPos(c, &coolenvpb.EndSimulateCarPosRequest{
+		CarId: "car123",
+	})
+	if err != nil {
+		panic(any(err))
+	}
+
 }
