1ef423d75245e5b25402e7eec4b0c582787c472a
HarryYang2002 <1260828355@qq.com>
2022-11-25 01:42:30 +0800

    blob配置

--- blob/main.go
+++ blob/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/namsral/flag"
 	"log"
 	blobpb "server/blob/api/gen/v1"
 	"server/blob/blob"
@@ -15,30 +16,34 @@ import (
 	"google.golang.org/grpc"
 )
 
+var addr = flag.String("addr", ":8083", "address to listen")
+var mongoURI = flag.String("mongo_uri", "your mongo uri", "mongo uri")
+var cosAddr = flag.String("cos_addr", "your cos addr", "cos address")
+var cosSecID = flag.String("cos_sec_id", "your cos secret ID", "cos secret id")
+var cosSecKey = flag.String("cos_sec_key", "your cos secret key", "cos secret key")
+
 func main() {
+	flag.Parse()
 	logger, err := server.NewZapLogger()
 	if err != nil {
 		log.Fatalf("cannot create logger: %v", err)
 	}
 
 	c := context.Background()
-	mongoClient, err := mongo.Connect(c, options.Client().ApplyURI("mongodb://localhost:27017/SZTURC?readPreference=primary&ssl=false"))
+	mongoClient, err := mongo.Connect(c, options.Client().ApplyURI(*mongoURI))
 	if err != nil {
 		logger.Fatal("cannot connect mongodb", zap.Error(err))
 	}
 	db := mongoClient.Database("SZTURC")
 
-	st, err := cos.NewService(
-		"your address",
-		"your sid",
-		"your skey")
+	st, err := cos.NewService(*cosAddr, *cosSecID, *cosSecKey)
 	if err != nil {
 		logger.Fatal("cannot create cos service", zap.Error(err))
 	}
 
 	logger.Sugar().Fatal(server.RunGRPCServer(&server.GRPCConfig{
 		Name:   "blob",
-		Addr:   ":8083",
+		Addr:   *addr,
 		Logger: logger,
 		RegisterFunc: func(s *grpc.Server) {
 			blobpb.RegisterBlobServiceServer(s, &blob.Service{
